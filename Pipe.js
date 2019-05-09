a8d3b1bd81e9aef473cc1e6a08993329fb4f11c2
Christian Hoffmann <chris.hoffmann84@gmail.com>
2019-05-09 17:42:43 +0200

    added single pipe

--- Pipe.js
+++ Pipe.js
@@ -1,4 +1,33 @@
 class Pipe extends GameObject
 {
-    
+    constructor()
+    {
+        super(width, 0);
+
+        let gap_height = Math.round(random(PIPE_MIN_GAP / 2, PIPE_MAX_GAP / 2));
+        let gap_pos = Math.round(random(gap_height + BOUNDS_THICKNESS, height - gap_height - BOUNDS_THICKNESS));
+
+        
+        this.gap = {
+            top: gap_pos - gap_height,
+            bottom: gap_pos + gap_height
+        };
+
+        console.log(this.gap);
+
+        this.vel.x = -PIPE_VELOCITY;
+    }
+
+    update()
+    {
+        super.update();
+    }
+
+    show()
+    {
+        noStroke();
+        fill(0);
+        rect(this.pos.x, 0, PIPE_WIDTH, this.gap.top);
+        rect(this.pos.x, this.gap.bottom, PIPE_WIDTH, height - this.gap.bottom);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -6,9 +6,17 @@ const CANVAS_HEIGHT = 600;
 const BOUNDS_THICKNESS = 20;
 const BIRD_RADIUS = 10;
 
+const PIPE_WIDTH = 40;
+const PIPE_MIN_GAP = 80;
+const PIPE_MAX_GAP = 160;
+const PIPE_MIN_DISTANCE = 100;
+const PIPE_MAX_DISTANCE = 200;
+const PIPE_VELOCITY = 3;
+
 const GRAVITY = 0.0981;
 const FLAP_FORCE = 4;
 
+let pipes;
 let renderer;
 let floor;
 let ceiling;
@@ -21,16 +29,32 @@ function setup()
 
     floor = new Bounds(true, BOUNDS_THICKNESS);
     ceiling = new Bounds(false, BOUNDS_THICKNESS);
+
+    pipes = [];
+    pipes.push(new Pipe());
+
     bird = new Bird(0.1 * width, 0.5 * height);
 
     renderer.addShowable(floor);
     renderer.addShowable(ceiling);
+
+    for (let pipe of pipes)
+    {
+        renderer.addShowable(pipe);
+    }
+
     renderer.addShowable(bird);
 }
 
 function draw() 
 {
+    for (let pipe of pipes)
+    {
+        pipe.update();
+    }
+
     bird.update();
+
     renderer.render();
 }
 
